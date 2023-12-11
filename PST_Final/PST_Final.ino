bb3ecca31b6588ba3584e91c0c429d9dad62d1b0
PastaCrepe <76557101+PastaCrepe@users.noreply.github.com>
2023-12-11 13:31:44 +0700

    Add files via upload

---
+++ PST_Final/PST_Final.ino
@@ -0,0 +1,83 @@
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+int timer1;
+int timer2;
+int shutter = 2;
+float Time;
+int flag1 = 0;
+int flag2 = 0;
+float distance = 5.0;
+float speed;
+int ir_s1 = A0;
+int ir_s2 = A1;
+const int buzzer = 13;
+
+void setup(){
+  pinMode(ir_s1, INPUT);
+  pinMode(ir_s2, INPUT);
+  pinMode(buzzer, OUTPUT);
+  
+  lcd.begin();
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Sistem Pengukur");
+  lcd.setCursor(0,1);
+  lcd.print("Kecepatan");
+  delay(2000);
+  lcd.clear();
+}
+
+void loop() {
+if(digitalRead (ir_s1) == LOW && flag1==0){
+  timer1 = millis(); 
+  flag1=1;
+  }
+
+if(digitalRead (ir_s2) == LOW && flag2==0){
+  timer2 = millis(); 
+  flag2=1;
+  }
+
+if (flag1==1 && flag2==1){
+     if(timer1 > timer2){
+      Time = timer1 - timer2;}
+else if(timer2 > timer1){
+  Time = timer2 - timer1;
+  }
+ Time=Time/1000;//convert millisecond to second
+ speed=(distance/Time);//v=d/t
+ speed=speed*3600;//multiply by seconds per hr
+ speed=speed/1000;//division by meters per Km
+}
+
+if(speed==0){ 
+lcd.setCursor(0, 1); 
+if(flag1==0 && flag2==0){lcd.print("No Vehicle");}
+                    else{lcd.print("Mencari...    ");} 
+}
+else{
+    lcd.clear(); 
+    lcd.setCursor(0, 0); 
+    lcd.print("Speed:");
+    lcd.print(speed,1);
+    lcd.print("Km/Hr  ");
+    lcd.setCursor(0, 1); 
+  if(speed > 50){
+    digitalWrite(shutter, HIGH);
+    delay(50);
+    digitalWrite(shutter, LOW);
+    lcd.print("Terlalu Cepat"); 
+    tone(buzzer, 1000); // Send 1KHz sound signal...
+    delay(2000);        // ...for 1 sec
+    noTone(buzzer);     // Stop sound...
+    lcd.clear();
+    }
+  else{
+    lcd.print("Kecepatan Aman"); 
+            }    
+    delay(1000);
+    speed = 0;
+    flag1 = 0;
+    flag2 = 0;    
+ }
+}
